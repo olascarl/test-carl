116621ef273654bb63f42f8f1212df0ec2b4a9a0
olascarl <115638968+olascarl@users.noreply.github.com>
2022-10-18 10:19:40 +0200

    v1

---
+++ Game Boy Advance - Pokemon Emerald - Interior Tilesets.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Game Boy Advance - Pokemon Emerald - Interior Tilesets" tilewidth="32" tileheight="32" tilecount="12288" columns="192">
+ <image source="../../../Downloads/Game Boy Advance - Pokemon Emerald - Interior Tilesets.png" width="6144" height="2048"/>
+</tileset>
